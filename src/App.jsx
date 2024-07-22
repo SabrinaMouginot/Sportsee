f41276c8f1552173af010cb04fc96218d4722803
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 10:37:55 +0200

    travail sur le fichier app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+
 function App() {
-  const { userId } = useParams();
+  // const { userId } = useParams();
 
   return (
-    <>
-      {userId}
-    </>
-  )
+    // <>
+    //   {userId}
+    // </>
+    <div className="App">
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/user/:userId" element={<Dashboard />} />
+        <Route path="/*" element={<div>Page not found</div>} />
+      </Routes>
+    </Router>
+  </div>
+  );
 }
 
 export default App
